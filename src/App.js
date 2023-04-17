43ee8d844b27e620d7c56b080e8645f570bdb742
Andreyhuey <andreyhuey777@gmail.com>
2023-04-17 23:56:30 +0100

    contact component removed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -6,7 +6,6 @@ import Support from "./components/Support";
 import AllInOne from "./components/AllInOne";
 import Pricing from "./components/Pricing";
 import Footer from "./components/Footer";
-// import Contact from "./components/Contact";
 import Tawk from "./components/Tawk.jsx";
 
 function App() {
@@ -18,7 +17,6 @@ function App() {
       <Support />
       <AllInOne />
       <Pricing />
-      {/* <Contact /> */}
       <Footer />
       <Tawk />
     </>
